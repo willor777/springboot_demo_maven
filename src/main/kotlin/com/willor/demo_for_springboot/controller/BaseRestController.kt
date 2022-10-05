023fdc25e28f63ef980672a5bcde6952fd34f609
willor777 <wcorwig777@gmail.com>
2022-10-05 17:37:55 -0400

    ktstocks test

--- src/main/kotlin/com/willor/demo_for_springboot/controller/BaseRestController.kt
+++ src/main/kotlin/com/willor/demo_for_springboot/controller/BaseRestController.kt
@@ -1,5 +1,6 @@
 package com.willor.demo_for_springboot.controller
 
+import com.willor.ktstockdata.KtStocks
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/base")
 class BaseRestController {
 
+    private val kt = KtStocks()
 
     @GetMapping("")
     fun welcome(): String{
         return "Welcome to the Base Endpoint"
+    }
+
+    @GetMapping("/quote")
+    fun quoteTest(): String{
+
+        return kt.marketData.getEtfQuote("SPY").toString()
+
     }
 }
